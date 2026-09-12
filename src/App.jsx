db8d98b22db808129301cb781688403e1cd14747
genie-ahn93 <ge2nie93@gmail.com>
2026-09-12 11:01:21 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -594,10 +594,8 @@ function MissionSubmitPage() {
                 </p>
 
                 <div className="notice-contact-box">
-                  <div className="notice-contact-copy">
-                    <span className="notice-contact-label">☎ 추가미션 참여 문의</span>
-                    <strong>시흥시청소년수련관 청소년활동사업팀</strong>
-                  </div>
+                  <div className="notice-contact-header">☎ 추가미션 참여 문의</div>
+                  <div className="notice-contact-org">시흥시청소년수련관 청소년활동사업팀</div>
                   <a className="notice-contact-phone" href="tel:0313151890">
                     031-315-1890 <span>(내선 1)</span>
                   </a>
